b10c3bffc415c741db10f569af273aa500baab4c
Emir <emirzeybek1029@gmail.com>
2025-12-20 01:31:01 +0300

    Version 1 of gameScreen is implemented

--- UntilTheLastColorStand/core/src/main/java/com/gameonjava/utlcs/gui/GameScreen.java
+++ UntilTheLastColorStand/core/src/main/java/com/gameonjava/utlcs/gui/GameScreen.java
@@ -3,7 +3,12 @@ package com.gameonjava.utlcs.gui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.gameonjava.utlcs.Main;
+import com.gameonjava.utlcs.backend.Player;
+import com.gameonjava.utlcs.backend.Game;
 
 public class GameScreen extends ScreenAdapter {
     private Main game;
@@ -19,5 +24,32 @@ public class GameScreen extends ScreenAdapter {
         multiplexer.addProcessor(hud.stage);
         multiplexer.addProcessor(new InputController(this));
         Gdx.input.setInputProcessor(multiplexer);
+        hud.updateStats(hud.backendGame.getCurrentPlayer(), Game.getCurrentTurn());
+
+        hud.getEndTurnBtn().addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+                hud.backendGame.nextTurn();
+
+                Player siradakiOyuncu = hud.backendGame.getCurrentPlayer();
+                int yeniTurSayisi = Game.getCurrentTurn();
+
+                hud.updateStats(siradakiOyuncu, yeniTurSayisi);
+            }
+        });
+    }
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.5f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // game.batch.begin();
+        // ...
+        // game.batch.end();
+
+        hud.render();
+    }
+    public void resize(int width, int height) {
+        hud.resize(width, height); 
     }
 }
