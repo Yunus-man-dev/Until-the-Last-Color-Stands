d3ea9d2c0daf3fba0f8a8f64ebee325965504f08
Yusuf Burak VURAL <burak.vural@ug.bilkent.edu.tr>
2025-12-22 23:14:53 +0300

    dark red fixed

--- UntilTheLastColorStand/core/src/main/java/com/gameonjava/utlcs/backend/civilization/DarkRed.java
+++ UntilTheLastColorStand/core/src/main/java/com/gameonjava/utlcs/backend/civilization/DarkRed.java
@@ -2,10 +2,8 @@ package com.gameonjava.utlcs.backend.civilization;
 
 public class DarkRed extends Red{
 
-    public DarkRed(String name){ {
+    public DarkRed(String name){
         super("Dark Red");
-
-
         this.civilizationName = name;
     }
     public DarkRed(){
